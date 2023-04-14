5c518942018d7bcbdcfa483dd6ac483565b1cb79
zadanabila <124870401+zadanabila@users.noreply.github.com>
2023-04-14 12:04:56 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'f13119b329fe4a50b7f6530470c514bc',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Zada Nabila',
+  domain: 'verce.zada.eu.org',
+  author: 'Zada Nabila',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
